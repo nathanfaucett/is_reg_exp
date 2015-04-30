271e6210ce71918e7db69853ed1fe71e3f718f3e
Nathan Faucett <nathanfaucett@gmail.com>
2015-04-30 12:54:20 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 var isObjectLike = require("is_object_like");
 
 
-var objectRegExpStr = "[object RegExp]",
-    objectToString = Object.prototype.toString;
+var objectToString = Object.prototype.toString;
 
 
-module.exports = function isRegExp(obj) {
-    return (isObjectLike(obj) && objectToString.call(obj) === objectRegExpStr) || false;
-};
+module.exports = isRegExp;
+
+
+function isRegExp(obj) {
+    return (
+        isObjectLike(obj) &&
+        objectToString.call(obj) === "[object RegExp]"
+    ) || false;
+}
